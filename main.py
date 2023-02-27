26ada15c04217e6c35d3cd2a2529ae2a1cabaeae
이용훈 <gagdoc@gmail.com>
2023-02-27 23:12:33 +0900

    날씨 알람

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+api_key = "9ca9d39577bc1e0fc821c96856376eff"
+
+weather_params = {
+    "lat": 37.566536,
+    "lon": 126.977966,
+    "appid": api_key,
+    "lang": "kr"
+
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+print(response.json())
+
